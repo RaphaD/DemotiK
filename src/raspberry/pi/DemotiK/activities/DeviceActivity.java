817543e839a0a7be35c603ff838c75f2b513283c
RaphaD <rduhen@ulb.ac.be>
2014-12-07 20:55:45 +0100

    [Device] Fully connected to the raspberry pi

--- src/raspberry/pi/DemotiK/activities/DeviceActivity.java
+++ src/raspberry/pi/DemotiK/activities/DeviceActivity.java
@@ -3,13 +3,19 @@ package raspberry.pi.DemotiK.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 import raspberry.pi.DemotiK.MainActivity;
 import raspberry.pi.DemotiK.R;
+
+import java.io.IOException;
 
 /**
  * Created by fums on 06.12.14.
  */
 public class DeviceActivity extends AbstractActivity {
+
+    ToggleButton mSonoToggle, mScreenToggle;
 
     @Override
     protected void onCreate(Bundle savedInstance) {
@@ -23,11 +29,15 @@ public class DeviceActivity extends AbstractActivity {
     @Override
     protected void initKeys() {
         mMenuButton = (Button) findViewById(R.id.menuDevice);
+        mSonoToggle = (ToggleButton) findViewById(R.id.toggleSono);
+        mScreenToggle = (ToggleButton) findViewById(R.id.toggleScreen);
     }
 
     @Override
     protected void addListeners() {
         mMenuButton.setOnClickListener(new MenuButtonListener());
+        mSonoToggle.setOnCheckedChangeListener(new SonoToggleListener());
+        mScreenToggle.setOnCheckedChangeListener(new ScreenToggleListener());
     }
 
     @Override
@@ -35,4 +45,39 @@ public class DeviceActivity extends AbstractActivity {
         Intent mainActivity = new Intent(DeviceActivity.this, MainActivity.class);
         startActivity(mainActivity);
     }
+
+    private class SonoToggleListener implements CompoundButton.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            String toWrite = "sono ";
+            if (isChecked)
+                toWrite += "on";
+            else
+                toWrite += "off";
+            try {
+                mSocket.writeInSocket(toWrite);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ScreenToggleListener implements CompoundButton.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            String toWrite = "screen ";
+            if (isChecked)
+                toWrite += "on";
+            else
+                toWrite += "off";
+            try {
+                mSocket.writeInSocket(toWrite);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
